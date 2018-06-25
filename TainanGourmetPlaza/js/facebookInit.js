5ca7a4ac963a7c7154a655e10721fdc916913999
SpicyBoyd <greenyu0510@gmail.com>
2018-06-26 00:15:51 +0800

    更新一版，詳細更動資料在：https://github.com/RainrainWu/Culture_Trip_In_Taiwan

---
+++ TainanGourmetPlaza/js/facebookInit.js
@@ -0,0 +1,8 @@
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.0&appId=1805550089560971&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ TainanGourmetPlaza/js/firebaseInit.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDbcUjl9UelfvBG2ZmpoFfwPnrylhCG_Mk",
+    authDomain: "radiant-entry-172510.firebaseapp.com",
+    databaseURL: "https://radiant-entry-172510.firebaseio.com",
+    projectId: "radiant-entry-172510",
+    storageBucket: "radiant-entry-172510.appspot.com",
+};
+firebase.initializeApp(config);
