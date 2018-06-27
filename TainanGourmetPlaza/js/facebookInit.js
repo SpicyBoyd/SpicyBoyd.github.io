c3199877c06ea1bbb38cf92c3c0bde63e9b2b0e4
SpicyBoyd <greenyu0510@gmail.com>
2018-06-28 01:38:32 +0800

    test

--- TainanGourmetPlaza/js/facebookInit.js
+++ TainanGourmetPlaza/js/facebookInit.js
@@ -3,6 +3,6 @@
     if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=1805550089560971&autoLogAppEvents=1';
+    js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.0&appId=1805550089560971&autoLogAppEvents=1';
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
